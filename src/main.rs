d33a3c446e25ad2fc11f5384dd66a065a2f708f6
oopsfoo <oopsfoo@users.noreply.github.com>
2021-06-10 13:48:33 +0800

    Add Command Line Options

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,51 @@
 use std::error::Error;
+use std::net::Ipv4Addr;
 use tokio::net::UdpSocket;
 use pretty_hex::*;
+use structopt::StructOpt;
 
+#[derive(Debug, StructOpt)]
+#[structopt(name = "example", about = "An example of StructOpt usage.")]
+struct Opt {
+    #[structopt(short, long)]
+    debug: bool,
+
+    #[structopt(short = "h", long = "host", default_value = "8.8.8.8")]
+    host: Ipv4Addr,
+
+    #[structopt(short = "p", long = "port", default_value = "53")]
+    port: u16,
+}
+
+struct Socks5Header {
+    header: [u8; 4],
+    ip: [u8; 4],
+    port: [u8; 2],
+}
+
+fn socks5Header() -> Vec<u8> {
+    let opt = Opt::from_args();
+    let dns_ip = opt.host;
+    let dns_bytes = dns_ip.octets();
+    let port_value = opt.port;
+    let port_bytes = port_value.to_be_bytes();
+    let socks5Header = Socks5Header{
+        header: [0x00, 0x00, 0x00, 0x01],
+        ip: dns_bytes,
+        port: port_bytes,
+    };
+    let mut full_header = Vec::new();
+    full_header.extend(socks5Header.header.iter().copied());
+    full_header.extend(socks5Header.ip.iter().copied());
+    full_header.extend(socks5Header.port.iter().copied());
+    assert_eq!(pretty_hex(&full_header), format!("{:?}", full_header.hex_dump()));
+    println!("{:?}", full_header.hex_dump());
+    return full_header
+}
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
-    let mut socks5_header = vec![0x00, 0x00, 0x00, 0x01];
-    assert_eq!(pretty_hex(&socks5_header), format!("{:?}", socks5_header.hex_dump()));
-    println!("{:?}", socks5_header.hex_dump());
-    let mut socks5_ip = vec![8, 8, 8, 8];
-    assert_eq!(pretty_hex(&socks5_ip), format!("{:?}", socks5_ip.hex_dump()));
-    println!("{:?}", socks5_ip.hex_dump());
-    let mut socks5_port = vec![0, 53];
-    assert_eq!(pretty_hex(&socks5_port), format!("{:?}", socks5_port.hex_dump()));
-    println!("{:?}", socks5_port.hex_dump());
-
+    let mut socks5_header = socks5Header();
     let sock = UdpSocket::bind("127.0.0.1:11223").await?;
     let mut buf = [0; 1024];
     loop {
@@ -24,8 +55,6 @@ async fn main() -> Result<(), Box<dyn Error>> {
         println!("{:?}", v.hex_dump());
         let mut pkt = vec![]  ;
         pkt.append(&mut socks5_header);
-        pkt.append(&mut socks5_ip);
-        pkt.append(&mut socks5_port);
         pkt.append(&mut v.to_vec());
 
         println!("----------------- request to socks server --------------");
